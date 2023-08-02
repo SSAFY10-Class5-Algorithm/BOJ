78b8c7a98f5b0365f571fb633c69110e3f468656
minginew <thsalsrl7531@gmail.com>
2023-08-02 14:34:03 +0900

    [JAVA] 1000 A+B

---
+++ 1000. A＋B/mingi/A＋B.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        Scanner scanner = new Scanner(System.in);
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+
+        System.out.println(a+b);
+    }
+}
